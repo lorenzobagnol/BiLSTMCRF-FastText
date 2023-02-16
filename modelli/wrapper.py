74d995ddf7d490a63043499acbd5f33e69bbe98f
lorenzo.bagnol <lorenzobgl@gmail.com>
2023-02-16 10:43:36 +0100

    first commit

---
+++ modelli/wrapper.py
@@ -0,0 +1,137 @@
+"""
+Wrapper class.
+"""
+from seqeval.metrics import f1_score
+import importlib
+from modelli.models import BiLSTMCRF, save_model, load_model
+import modelli.preprocessing 
+importlib.reload(modelli.preprocessing)
+from modelli.preprocessing import IndexTransformer
+from modelli.tagger import Tagger
+from modelli.utils import filter_embeddings
+
+
+class Sequence(object):
+
+    def __init__(self,
+                 word_embedding_dim=100,
+                 char_embedding_dim=25,
+                 word_lstm_size=100,
+                 char_lstm_size=25,
+                 fc_dim=100,
+                 dropout=0.5,
+                 embeddings=None,
+                 use_char=True,
+                 use_crf=True,
+                 initial_vocab=None,
+                 optimizer='adam'):
+
+        self.model = None
+        self.p = None
+        self.tagger = None
+
+        self.word_embedding_dim = word_embedding_dim
+        self.char_embedding_dim = char_embedding_dim
+        self.word_lstm_size = word_lstm_size
+        self.char_lstm_size = char_lstm_size
+        self.fc_dim = fc_dim
+        self.dropout = dropout
+        self.embeddings = embeddings
+        self.use_char = use_char
+        self.use_crf = use_crf
+        self.initial_vocab = initial_vocab
+        self.optimizer = optimizer
+
+    def fit_vocab(self, x_train, y_train):
+        p = IndexTransformer(initial_vocab=self.initial_vocab, use_char=self.use_char, lower=False)
+        p.build_vocab(x_train, y_train)
+        embeddings = self.embeddings.vectors_vocab
+
+        model = BiLSTMCRF(char_vocab_size=p.char_vocab_size,
+                          word_vocab_size=p.word_vocab_size,
+                          num_labels=p.label_size,
+                          word_embedding_dim=self.word_embedding_dim,
+                          char_embedding_dim=self.char_embedding_dim,
+                          word_lstm_size=self.word_lstm_size,
+                          char_lstm_size=self.char_lstm_size,
+                          fc_dim=self.fc_dim,
+                          dropout=self.dropout,
+                          embeddings=embeddings,
+                          use_char=self.use_char,
+                          use_crf=self.use_crf)
+        model, self.loss = model.build()
+        model.compile(loss=self.loss, optimizer=self.optimizer)
+        self.p = p
+        self.model=model
+
+    def predict(self, x_test):
+        """Returns the prediction of the model on the given test data.
+
+        Args:
+            x_test : array-like, shape = (n_samples, sent_length)
+            Test samples.
+
+        Returns:
+            y_pred : array-like, shape = (n_smaples, sent_length)
+            Prediction labels for x.
+        """
+        if self.model:
+            lengths = map(len, x_test)
+            x_test = self.p.transform(x_test)
+            y_pred = self.model.predict(x_test)
+            y_pred = self.p.inverse_transform(y_pred, lengths)
+            return y_pred 
+        else:
+            raise OSError('Could not find a model. Call load(dir_path).')
+
+    def score(self, x_test, y_test):
+        """Returns the f1-micro score on the given test data and labels.
+
+        Args:
+            x_test : array-like, shape = (n_samples, sent_length)
+            Test samples.
+
+            y_test : array-like, shape = (n_samples, sent_length)
+            True labels for x.
+
+        Returns:
+            score : float, f1-micro score.
+        """
+        if self.model:
+            x_test = self.p.transform(x_test)
+            lengths = map(len, y_test)
+            y_pred = self.model.predict(x_test)
+            y_pred = self.p.inverse_transform(y_pred, lengths)
+            score = f1_score(y_test, y_pred)
+            return score
+        else:
+            raise OSError('Could not find a model. Call load(dir_path).')
+
+    def analyze(self, text, tokenizer=str.split):
+        """Analyze text and return pretty format.
+
+        Args:
+            text: string, the input text.
+            tokenizer: Tokenize input sentence. Default tokenizer is `str.split`.
+
+        Returns:
+            res: dict.
+        """
+        if not self.tagger:
+            self.tagger = Tagger(self.model,
+                                 preprocessor=self.p,
+                                 tokenizer=tokenizer)
+
+        return self.tagger.analyze(text)
+
+    def save(self, weights_file, params_file, preprocessor_file):
+        self.p.save(preprocessor_file)
+        save_model(self.model, weights_file, params_file)
+
+    @classmethod
+    def load(cls, weights_file, params_file, preprocessor_file):
+        self = cls()
+        self.p = IndexTransformer.load(preprocessor_file)
+        self.model = load_model(weights_file, params_file)
+
+        return self
